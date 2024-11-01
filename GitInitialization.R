2f4e56bc7c64af516af7691357fbe5713f5d91a8
Stefan Graf <stgr@esri-de.com>
2024-11-01 23:51:48 +0100

    progress in course

--- GitInitialization.R
+++ GitInitialization.R
@@ -2,7 +2,7 @@
 system("git init")
 
 # Add remote GitHub repository (replace the URL with your GitHub repository link)
-system('git remote add origin https://github.com/greentrea/CourseRB')
+system('git remote add origin https://github.com/greentrea/CourseRB.git')
 
 # Add all files to staging
 system("git add .")
@@ -12,3 +12,6 @@ system('git commit -m "Initial commit with course materials and renv setup"')
 
 # Push the local repository to GitHub
 system('git push -u origin master')
+
+
+token <- auth_code()
